ba54d03d1ce65f8f8a1f8e30d62b3d6b63d37ba8
ARTEM BOYKO <boyko.a.v@bk.ru>
2024-04-04 16:07:22 +0700

    serializers

--- travels/serializers.py
+++ travels/serializers.py
@@ -1,11 +1,6 @@
 from rest_framework import serializers
 from .models import Travel, Country
-
-class TravelSerializer(serializers.ModelSerializer):
-    travel_count = serializers.IntegerField(read_only=True)
-    class Meta:
-        model = Travel
-        fields = '__all__'
+from users.serializers import CitySerializer
 
 class CountrySerializer(serializers.ModelSerializer):
     travel_count = serializers.IntegerField(read_only=True)
@@ -13,3 +8,11 @@ class CountrySerializer(serializers.ModelSerializer):
     class Meta:
         model = Country
         fields = ['id', 'name', 'image', 'travel_count']
+
+class TravelSerializer(serializers.ModelSerializer):
+    travel_count = serializers.IntegerField(read_only=True)
+    from_city = CitySerializer()
+    country = CountrySerializer()
+    class Meta:
+        model = Travel
+        fields = '__all__'
--- users/serializers.py
+++ users/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import CustomUser, Interests, Habits
+from .models import CustomUser, Interests, Habits, City
 
 class CustomUserSerializer(serializers.ModelSerializer):
     class Meta:
@@ -14,4 +14,9 @@ class InterestsSerializer(serializers.ModelSerializer):
 class HabitsSerializer(serializers.ModelSerializer):
     class Meta:
         model = Habits
+        fields = '__all__'
+
+class CitySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = City
         fields = '__all__'
