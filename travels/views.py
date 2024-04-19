332c679a781cbc0b58ff122fe10a52711f98b877
ARTEM BOYKO <boyko.a.v@bk.ru>
2024-04-19 18:32:28 +0700

    countries_from_moscow

--- travels/views.py
+++ travels/views.py
@@ -19,6 +19,13 @@ class TravelViewSet(viewsets.ModelViewSet):
 
         return Response({'total_travel_count': total_count})
 
+    @action(detail=False, methods=['get'])
+    def countries_from_moscow(self, request):
+        countries_from_moscow = Country.objects.filter(travels__from_city__name='Москва').distinct()
+        serializer = CountrySerializer(countries_from_moscow, many=True)
+        return Response(serializer.data)
+
+
 class CountryViewSet(viewsets.ModelViewSet):
     queryset = Country.objects.annotate(travel_count=Count('travels'))
     serializer_class = CountrySerializer
@@ -27,9 +34,3 @@ class CountryViewSet(viewsets.ModelViewSet):
     def total_country_count(self, request):
         total_count = Country.objects.count()
         return Response({'total_country_count': total_count})
-    
-    @action(detail=False, methods=['get'])
-    def countries_from_moscow(self, request):
-        countries_from_moscow = Country.objects.filter(travels__from_city__name='Москва').distinct()
-        serializer = CountrySerializer(countries_from_moscow, many=True)
-        return Response(serializer.data)
